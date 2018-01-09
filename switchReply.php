bc8c8386f78a27c41ee79ea425829cedfe76a4fb
umxa <35246334+umxa@users.noreply.github.com>
2018-01-09 15:00:57 +0800

    Update switchReply.php

--- switchReply.php
+++ switchReply.php
@@ -1,6 +1,9 @@
 <?php
  $json_str = file_get_contents('php://input'); //接收REQUEST的BODY
  $json_obj = json_decode($json_str); //轉JSON格式
+$myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
+ fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前加入\xEF\xBB\xBF轉成utf8格式
+ fclose($myfile);
  //產生回傳給line server的格式
  $sender_userid = $json_obj->events[0]->source->userId;
  $sender_txt = $json_obj->events[0]->message->text;
@@ -72,7 +75,7 @@
 				)
 			);
         		break;
-                case "button":
+		 case "button":
 			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
         		$response = array (
 				"replyToken" => $sender_replyToken,
@@ -102,14 +105,10 @@
 				)
 			);
         		break;
-
  }
- $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
- fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
- fclose($myfile);
  //回傳給line server
  $header[] = "Content-Type: application/json";
- $header[] = "Authorization: Bearer Ge6chAnpmdNZyieCLYHD9nnqDTF6rB6oYp6saZMj6kw/aj7VbDEtpPOALO58ueXseRk6UbEZW3KZPQ1u9nTOGbGkW1f4X8EhUKAGK8PhH0GDqm7KzwkoOvVjLXIAuDKVEjHseX7jHNHHqXm+JYxyigdB04t89/1O/w1cDnyilFU=";
+ $header[] = "Authorization: Bearer n4mZIQp9UqWXhCEgIg1fLmyjUeDMgCe/bF+4EOBDZ7fGscOgNGFsHTr3fGco/E7A5hq7A7jiDszSCk/j3pVVPbx7nf0E+FKe5jX6syQGOxO7kwp5lmZ3zRES1qxceq/N+/E9Qy5gSDbBx56l8sScTwdB04t89/1O/w1cDnyilFU=";
  $ch = curl_init($line_server_url);                                                                      
  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
