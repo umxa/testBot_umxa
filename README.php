77da6a6c95f64475c51c01b3a53f30e43a3dc5dd
umxa <35246334+umxa@users.noreply.github.com>
2018-01-09 11:36:39 +0800

    get line message

--- README.php
+++ README.php
@@ -1,3 +1,9 @@
 <?php
-echo "GGGGGGGGGGGGGGGGG"
+  echo "GGGGGGGGGGGGGGGGG"
+  $json_str = file_get_contents('php://input');//接收request的body
+  $json_obj = json_decode($json_str);//轉JSON格式
+  $myfile = fopen("log.txt","w+") or die("Unable to poen file !");
+  fwrite($myfile,"\xEF\xBB\xBF".$json_str);//在字串前加入\xEF\XBB\xBF轉成utf8格式
+  fclose($myfile);
+  
 ?>
