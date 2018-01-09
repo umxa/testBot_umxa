79d586e0ebcf78c3ea874aaa3feb2b75974c576b
umxa <35246334+umxa@users.noreply.github.com>
2018-01-09 14:44:09 +0800

    Delete README.php

--- README.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $json_str = file_get_contents('php://input');//接收request的body
-  $json_obj = json_decode($json_str);//轉JSON格式
-  $myfile1 = fopen("log.txt","w+") or die("Unable to open file !");
-  fwrite($myfile1,"\xEF\xBB\xBF".$json_str);//在字串前加入\xEF\XBB\xBF轉成utf8格式
-  fclose($myfile1);
-  
-  
-?>
