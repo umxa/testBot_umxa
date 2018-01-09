e4c294319dc79100389bc04a61ed196b104121a2
umxa <35246334+umxa@users.noreply.github.com>
2018-01-09 11:44:34 +0800

    Update README.php

--- README.php
+++ README.php
@@ -1,4 +1,5 @@
 <?php
+  echo "aaaaaaaaaaaaaaa"
   $json_str = file_get_contents('php://input');//接收request的body
   $json_obj = json_decode($json_str);//轉JSON格式
   $myfile = fopen("log.txt","w+") or die("Unable to open file !");
