f213192a3a78ebc1510411ac4749f29cf84b8738
umxa <35246334+umxa@users.noreply.github.com>
2018-01-09 15:58:21 +0800

    Update switchReply.php

--- switchReply.php
+++ switchReply.php
@@ -15,7 +15,7 @@ $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個lo
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-				'Authorization: Bearer n4mZIQp9UqWXhCEgIg1fLmyjUeDMgCe/bF+4EOBDZ7fGscOgNGFsHTr3fGco/E7A5hq7A7jiDszSCk/j3pVVPbx7nf0E+FKe5jX6syQGOxO7kwp5lmZ3zRES1qxceq/N+/E9Qy5gSDbBx56l8sScTwdB04t89/1O/w1cDnyilFU=',
+				'Authorization: Bearer h9fxuK5CCnKYJn6e24KGaDxGKp/9EjKy1TkGjuL9DU9WiMtx6yaIcKCtT63sXfpWeRk6UbEZW3KZPQ1u9nTOGbGkW1f4X8EhUKAGK8PhH0E/6u5nyGoBqj4ftD5+xhqZve+mKdCP1DSwRwUK0eJ9bwdB04t89/1O/w1cDnyilFU=',
 			));
 				
 			$json_content = curl_exec($ch);
@@ -25,7 +25,7 @@ $imagefile = fopen($objID.".jpeg", "w+") or die("Unable to open file!"); //設�
 			fclose($imagefile);
  //回傳給line server
  $header[] = "Content-Type: application/json";
- $header[] = "Authorization: Bearer n4mZIQp9UqWXhCEgIg1fLmyjUeDMgCe/bF+4EOBDZ7fGscOgNGFsHTr3fGco/E7A5hq7A7jiDszSCk/j3pVVPbx7nf0E+FKe5jX6syQGOxO7kwp5lmZ3zRES1qxceq/N+/E9Qy5gSDbBx56l8sScTwdB04t89/1O/w1cDnyilFU=";
+ $header[] = "Authorization: Bearer h9fxuK5CCnKYJn6e24KGaDxGKp/9EjKy1TkGjuL9DU9WiMtx6yaIcKCtT63sXfpWeRk6UbEZW3KZPQ1u9nTOGbGkW1f4X8EhUKAGK8PhH0E/6u5nyGoBqj4ftD5+xhqZve+mKdCP1DSwRwUK0eJ9bwdB04t89/1O/w1cDnyilFU=";
  $ch = curl_init($line_server_url);                                                                      
  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
