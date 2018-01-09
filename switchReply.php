56c24517ec5f9ad76679d0f28a812f5b16f6ce1a
umxa <35246334+umxa@users.noreply.github.com>
2018-01-09 14:45:13 +0800

    Update switchReply.php

--- switchReply.php
+++ switchReply.php
@@ -50,7 +50,7 @@
  fclose($myfile);
  //回傳給line server
  $header[] = "Content-Type: application/json";
- $header[] = "Authorization: Bearer n4mZIQp9UqWXhCEgIg1fLmyjUeDMgCe/bF+4EOBDZ7fGscOgNGFsHTr3fGco/E7A5hq7A7jiDszSCk/j3pVVPbx7nf0E+FKe5jX6syQGOxO7kwp5lmZ3zRES1qxceq/N+/E9Qy5gSDbBx56l8sScTwdB04t89/1O/w1cDnyilFU=";
+ $header[] = "Authorization: Bearer Ge6chAnpmdNZyieCLYHD9nnqDTF6rB6oYp6saZMj6kw/aj7VbDEtpPOALO58ueXseRk6UbEZW3KZPQ1u9nTOGbGkW1f4X8EhUKAGK8PhH0GDqm7KzwkoOvVjLXIAuDKVEjHseX7jHNHHqXm+JYxyigdB04t89/1O/w1cDnyilFU=";
  $ch = curl_init($line_server_url);                                                                      
  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
